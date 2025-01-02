46d04ca0d239ef51e791f408d9b394769edf4d58
Bigmac196 <lm4301001@gmail.com>
2025-01-02 00:14:05 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PhotoGallery(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class PhotoGallery extends StatefulWidget {
+  const PhotoGallery({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _PhotoGalleryState createState() => _PhotoGalleryState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PhotoGalleryState extends State<PhotoGallery> {
+  List<AssetEntity> _photos = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _loadPhotos();
+  }
+
+  // Request permission and load photos from the gallery
+  Future<void> _loadPhotos() async {
+    final permissionStatus = await Permission.photos.request();
+    if (permissionStatus.isGranted) {
+      final assets = await PhotoManager.getAssetPathList(onlyAll: true);
+      final recentAlbum = await assets[0].getAssetListRange(start: 0, end: 100);
+      setState(() {
+        _photos = recentAlbum;
+      });
+    } else {
+      // If permission is not granted, show a message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Permission to access photos denied.')),
+      );
+    }
+  }
+
+  // Take a photo and reload the gallery
+  Future<void> _takePhoto() async {
+    final picker = ImagePicker();
+    final photo = await picker.pickImage(source: ImageSource.camera);
+    if (photo != null) {
+      // ignore: unused_local_variable
+      final file = await photo.saveToFile();
+      _loadPhotos(); // Reload photos after taking a new one
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      appBar: AppBar(title: Text('Photo Gallery')),
+      body: Column(
+        children: [
+          // Button to take a photo
+          ElevatedButton(
+            onPressed: _takePhoto,
+            child: Text('Take Photo'),
+          ),
+          // Button to reload photos from the gallery
+          ElevatedButton(
+            onPressed: _loadPhotos,
+            child: Text('Reload Photos'),
+          ),
+          // Display gallery photos in a grid view
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: _photos.length,
+              itemBuilder: (context, index) {
+                return FutureBuilder<Widget>(
+                  future: _getImage(_photos[index]),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return snapshot.data!;
+                    }
+                    return CircularProgressIndicator();
+                  },
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  // Convert photo to image widget
+  Future<Widget> _getImage(AssetEntity asset) async {
+    final file = await asset.file;
+    return Image.file(file!, fit: BoxFit.cover);
+  }
+}
+
+extension on XFile {
+  saveToFile() {}
 }
